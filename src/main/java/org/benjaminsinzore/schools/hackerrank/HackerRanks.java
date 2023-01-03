1e526222bf51e7d740eba43c47c200c9eb68be18
benjiesinzore <benjaminsinzore@gmail.com>
2023-01-04 02:46:37 +0300

    RandomTask

--- src/main/java/org/benjaminsinzore/schools/hackerrank/HackerRanks.java
+++ src/main/java/org/benjaminsinzore/schools/hackerrank/HackerRanks.java
@@ -28,17 +28,77 @@ public class HackerRanks {
     public static void projectManager(List<String> list){
 
         int count = 0;
+        StringBuilder sS = new StringBuilder();
         for (String ss : list){
 
             count++;
             String[] lst = ss.split("");
 
             if ("Y".equals(lst[0]) && "Y".equals(lst[1]) && "Y".equals(lst[2])) {
-                System.out.println(Arrays.toString(lst)+"\n");
+
+                sS.append(count);
             }
 
         }
-        System.out.println(count);
+
+        String[] mySs = sS.toString().split("");
+
+        int[] newInt = new int[mySs.length];
+        int i = 0;
+        for (String myInt : mySs){
+            newInt[i] = Integer.parseInt(myInt);
+            i++;
+        }
+
+        StringBuilder myStr = new StringBuilder();
+        for (int value : newInt) {
+
+            for (int k = 1; k < newInt.length; k++) {
+
+
+                if (value + 1 == newInt[k]) {
+
+                    myStr.append(value);
+                }
+
+            }
+
+        }
+
+        if (newInt[newInt.length-1] -1 == newInt[newInt.length-2]){
+            myStr.append(newInt[newInt.length-1]);
+        }
+
+
+        String[] myStt = myStr.toString().split("");
+        StringBuilder fin = new StringBuilder();
+        for (String st: myStt) {
+            switch (st){
+                case "1":
+                    fin.append("Monday ");
+                    break;
+                case "2":
+                    fin.append("Tuesday ");
+                    break;
+                case "3":
+                    fin.append("Wednesday ");
+                    break;
+                case "4":
+                    fin.append("Thursday ");
+                    break;
+                case "5":
+                    fin.append("Friday ");
+                    break;
+                case "6":
+                    fin.append("Saturday ");
+                    break;
+                case "7":
+                    fin.append("Sunday");
+                    break;
+            }
+        }
+
+        System.out.println(fin);
 
     }
 }
